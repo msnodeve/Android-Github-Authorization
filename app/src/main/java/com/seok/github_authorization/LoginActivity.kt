3b604f44f7fcf0485c89eea6372d8e471bd3f4cb
msnodeve <msnodeve@gmail.com>
2019-07-31 10:06:53 +0900

    github token login

--- app/src/main/java/com/seok/github_authorization/LoginActivity.kt
+++ app/src/main/java/com/seok/github_authorization/LoginActivity.kt
@@ -1,12 +1,35 @@
 package com.seok.github_authorization
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.HttpUrl
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_btn_auth.setOnClickListener {
+            signIn()
+        }
+
+    }
+    private fun signIn() {
+        var httpUrl :HttpUrl = HttpUrl.Builder()
+            .scheme("https")
+            .host("github.com")
+            .addPathSegment("login")
+            .addPathSegment("oauth")
+            .addPathSegment("authorize")
+            .addQueryParameter("client_id", getString(R.string.github_app_id))
+            .addQueryParameter("scope", "user:email")
+            .build()
+        var intent : Intent = Intent(Intent.ACTION_VIEW, Uri.parse(httpUrl.toString()))
+        startActivityForResult(intent, StatusCode.REQUEST_GITHUB_REDIRECT)
     }
 }
--- app/src/main/java/com/seok/github_authorization/RedirectedActivity.kt
+++ app/src/main/java/com/seok/github_authorization/RedirectedActivity.kt
@@ -1,13 +1,22 @@
 package com.seok.github_authorization
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class RedirectedActivity : Activity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_redirected)
+        var uri : Uri = intent.data
+        if(uri != null && uri.toString().startsWith(getString(R.string.github_app_url))){
+            var intent : Intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("code", uri.getQueryParameter("code"))
+            intent.putExtra("state", uri.getQueryParameter("state"))
+            startActivity(intent)
+            finish()
+        }
     }
 }
